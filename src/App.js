06e833d8959aa8ee238b497b3c3d4f25513a16ff
MauricioFernanddes <mauriciojrfs@hotmail.com>
2023-03-25 14:12:53 -0300

    feat: add página inicial com retorno da api e modal com detalhes dos quadrinhos

--- src/App.js
+++ src/App.js
@@ -1,37 +1,98 @@
-import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { fetchComics } from './marvel';
+import { useState, useEffect } from "react";
+import './index.css';
+
 
 function App() {
-  const [comics, setComics] = useState('')
+  const [comics, setComics] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedComic, setSelectedComic] = useState(null);
+
+  const fetchComics = async () => {
+    const timestamp = 1679627411764;
+    const hash = "9770dbd31f88df8402b6003f18465292";
+    const apiKey = "28d1eff87f424ef31391c5eb9ff016b7";
+    const url = `http://gateway.marvel.com/v1/public/comics?ts=${timestamp}&apikey=${apiKey}&hash=${hash}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.data && data.data.results) {
+      setComics(data.data.results);
+    }
+  };
 
   useEffect(() => {
-    getComics()
-  }, [])
+    fetchComics();
+  }, []);
 
-  const getComics = async () => {
-    const result = await fetchComics()
-    setComics(result)
-  }
-  
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    const searchValue = event.target.elements.search.value;
+    const timestamp = 1679627411764;
+    const hash = "9770dbd31f88df8402b6003f18465292";
+    const apiKey = "28d1eff87f424ef31391c5eb9ff016b7";
+    const url = `http://gateway.marvel.com/v1/public/comics?titleStartsWith=${searchValue}&ts=${timestamp}&apikey=${apiKey}&hash=${hash}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.data && data.data.results) {
+      setComics(data.data.results);
+    }
+  };
+
+  const handleOpenModal = (comic) => {
+    setSelectedComic(comic);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedComic(null);
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={comics} alt="comic img"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSearch}>
+        <label>
+          Search comics by title:
+          <input type="text" name="search" />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {comics.map((comic) => (
+          <li key={comic.id}>
+            <button onClick={() => handleOpenModal(comic)}>
+              <img
+                src={`${comic.thumbnail.path}/portrait_uncanny.${comic.thumbnail.extension}`}
+                alt={comic.title}
+              />
+              <p>{comic.title}</p>
+            </button>
+          </li>
+        ))}
+      </ul>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCloseModal}>
+              &times;
+            </span>
+            <h2>{selectedComic.title}</h2>
+            <img
+              src={`${selectedComic.thumbnail.path}/portrait_uncanny.${selectedComic.thumbnail.extension}`}
+              alt={selectedComic.title}
+            />
+            <p>{selectedComic.description}</p>
+            <ul>
+              {selectedComic.characters.items.map((character) => (
+                <li key={character.name}>{character.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/marvel.js
+++ src/marvel.js
@@ -1,18 +1,29 @@
-const ts = 1679627411764
-const hash = "9770dbd31f88df8402b6003f18465292"
-const apiKey = "28d1eff87f424ef31391c5eb9ff016b7"
+// const ts = 1679627411764
+// const hash = "9770dbd31f88df8402b6003f18465292"
+// const apiKey = "28d1eff87f424ef31391c5eb9ff016b7"
 
-export const fetchComics = async () => {
-    const comics = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${apiKey}&hash=${hash}`).then(res => res.json())
-    // if(!comics.data.results[0].images[0]){
-    //     return "https://abravidro.org.br/wp-content/uploads/2015/04/sem-imagem7.jpg"
-    // }s
-    console.log(comics)
-    const filtered = comics.data.results.filter(comicWithImage => !!comicWithImage.images[0])
-    console.log(filtered)
-    const imagens = filtered.map(comic => comic.images[0].path)
-    console.log(imagens)
-    // const xmen = comics.data.results[0].images[0]
-    // // console.log(`${xmen.path}.${xmen.extension}`)
-    // return `${xmen.path}.${xmen.extension}`
-}
+// export const fetchComics = async () => {
+//     const comics = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${apiKey}&hash=${hash}`).then(res => res.json())
+//     // if(!comics.data.results[0].images[0]){
+//     //     return "https://abravidro.org.br/wp-content/uploads/2015/04/sem-imagem7.jpg"
+//     // }s
+//     console.log(comics)
+//     const filtered = comics.data.results.filter(comicWithImage => !!comicWithImage.images[0])
+//     console.log(filtered)
+//     const imagens = filtered.map(comic => comic.images[0].path)
+//     console.log(imagens)
+//     // const xmen = comics.data.results[0].images[0]
+//     // // console.log(`${xmen.path}.${xmen.extension}`)
+//     // return `${xmen.path}.${xmen.extension}`
+// }
+// import md5 from 'md5';
+
+// const publicKey = '28d1eff87f424ef31391c5eb9ff016b7';
+// const privateKey = 'de1a26ee31c4db12e06bbd71ca5f0e1b6099d0ab';
+// const timestamp = Date.now();
+// const hash = md5(timestamp + privateKey + publicKey);
+
+// fetch(`https://gateway.marvel.com/v1/public/comics?apikey=${publicKey}&ts=${timestamp}&hash=${hash}`)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
